9e88ab1bda38354a018653c38bea888e6726d92c
Bryan Karaffa <bryankaraffa@gmail.com>
2017-01-10 09:25:46 -0800

    Update default.rb
    Removing the functionality that ADDS the `SERVERID` of the backend server that handled the response to prevent the client side / AEM from being able to save it and request a specific backend instance to handle the request.

--- recipes/default.rb
+++ recipes/default.rb
@@ -143,7 +143,7 @@ if node['haproxy']['httpchk']
 end
 
 if node['rs-haproxy']['session_stickiness']
-  node.default['haproxy']['config']['defaults']['cookie'] = 'SERVERID insert indirect nocache'
+  node.default['haproxy']['config']['defaults']['cookie'] = 'SERVERID nocache'
 end
 
 # Confirm that rsyslog is installed.
